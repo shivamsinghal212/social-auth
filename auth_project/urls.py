ed4b7ff15c76db49ce09a274fa4650a669e1f593
shivam <shivam.singhal212@gmail.com>
2018-03-29 20:12:26 +0530

    #ID-02 Added Basic Registration.

--- auth_project/urls.py
+++ auth_project/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pages.urls'))
+    path('', include('users.urls')),
+    path('', include('pages.urls')),
+    path('',include('django.contrib.auth.urls'))
 ]
